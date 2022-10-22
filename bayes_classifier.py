126fc0271e7ad9ab4cd70a2a7b21d4c4b884d718
Christos <46033926+chrigiach@users.noreply.github.com>
2022-10-22 19:12:40 +0300

    Create bayes_classifier.py

---
+++ bayes_classifier.py
@@ -0,0 +1,9 @@
+# Pattern recognition course | ECE AUTH | 1st project
+# Giachoudis Christos | 9912 | Winter semester 2022
+
+# A priori knowledge
+m1 = [0.4, 0.8] # mean values for class ω_1
+m2 = [1.5, 2.7] # mean values for class ω_2
+Sigma = [[1.5, 0],[0, 0.8]] # covariance matrix for ω_1 and ω_2
+p_1 = 0.95 # a priori probability for class ω_1
+p_2 = 0.05 # a priori probability for class ω_2
